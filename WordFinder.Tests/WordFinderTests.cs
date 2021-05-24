6ddda2c2b88f10a16cf40032742bd7b470bbb49e
lvergnory <lvergnory@qubeyond.com>
2021-05-24 19:47:53 -0300

    word finder

--- WordFinder.Tests/WordFinderTests.cs
+++ WordFinder.Tests/WordFinderTests.cs
@@ -2,8 +2,6 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using WordFinder.Validation;
 
 namespace Tests
@@ -240,108 +238,5 @@ namespace Tests
             Assert.Throws<Exception>(() => wordFinderValidations.ValidateMatrix(matrix));
 
         }
-
-        [Test]
-        public void Test()
-        {
-            string html = "truongpm<b class=2><i>bold italic</i></b><b>bold</b><i>italic</i><html>";
-                       
-            var parts = html[1];
-
-
-            List<string> tagsToFind = new List<string>() { "i", "b", "html" };
-            Dictionary<string, string> htmlElementsFound = new Dictionary<string, string>();
-            Dictionary<string, bool> htmlElementsMatched = tagsToFind.ToDictionary(x => x, x => false);
-
-            int lastIndexMatch = 0;
-
-            foreach(var tag in tagsToFind)
-            {
-                if (!htmlElementsFound.ContainsKey(tag))
-                {
-                    lastIndexMatch = FindTagsInElement(html, tag, htmlElementsFound, lastIndexMatch);
-                    if (lastIndexMatch != -1)
-                        htmlElementsMatched[tag] = true;
-                }
-                else
-                {
-                    htmlElementsMatched[tag] = true;
-                }
-            }
-
-            var a = htmlElementsMatched;
-
-        }       
-
-        private int FindTagsInElement(string html, string tag, Dictionary<string, string> htmlElementFound, int actualIndex)
-        {
-            var lenght = html.Length;
-            bool enclosedScopeFound = false;
-            StringBuilder htmlElement = new StringBuilder();
-
-            for(; actualIndex < lenght; actualIndex++)
-            {
-                if (enclosedScopeFound && IsValueValidForScope(html, actualIndex))
-                {
-                    htmlElement.Append(html[actualIndex]);
-                }
-                else if (enclosedScopeFound)
-                {
-                    var htmlElementString = htmlElement.ToString();
-                    htmlElement.Clear();
-                    htmlElementFound[htmlElementString] = htmlElementString;
-
-                    if (string.Equals(htmlElementString, tag, StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        return actualIndex;
-                    }
-
-                    enclosedScopeFound = false;
-
-                }
-                else if (IsNewHtmlScope(html, actualIndex, lenght))
-                {
-                    enclosedScopeFound = true;
-                }
-            }
-
-            return -1;
-
-        }
-
-        private static bool IsNewHtmlScope(string html, int actualIndex, int lenght)
-        {
-            return char.Equals(html[actualIndex], '<') && (actualIndex + 1) != lenght && !char.Equals(html[actualIndex + 1], '/');
-        }
-
-        private static bool IsValueValidForScope(string html, int actualIndex)
-        {
-            return !char.IsWhiteSpace(html[actualIndex]) && !char.Equals(html[actualIndex], '/') && !char.Equals(html[actualIndex], '>');
-        }
-
-        /*static int binarySearch(string[] arr, String x)
-        {
-            int l = 0, r = arr.Length - 1;
-            while (l <= r)
-            {
-                int m = l + (r - l) / 2;
-
-                int res = x.CompareTo(arr[m]);
-
-                // Check if x is present at mid 
-                if (res == 0)
-                    return m;
-
-                // If x greater, ignore left half 
-                if (res > 0)
-                    l = m + 1;
-
-                // If x is smaller, ignore right half 
-                else
-                    r = m - 1;
-            }
-
-            return -1;
-        }*/
     }
 }
